afa90dc493e35d0c48f85dccf78b7615209072de
Ljubomir <ljupcedavitkov14@gmail.com>
2024-09-25 00:40:22 +0200

    Login

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,15 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    // apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    apiKey: "AIzaSyD8O5IhhKw8wOGwNgDdaQE9L1HKRWpM2SU",
     authDomain: "tutorialsept.firebaseapp.com",
     projectId: "tutorialsept",
     storageBucket: "tutorialsept.appspot.com",
@@ -15,3 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
