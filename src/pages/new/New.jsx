958336206a50c0c81af9b1ae02703b3f038f0638
Ljubomir <ljupcedavitkov14@gmail.com>
2024-09-26 18:47:33 +0200

    Create data

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,57 +2,93 @@ import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-import { useState } from "react";
+import {useState} from "react";
 
-const New = ({ inputs, title }) => {
-  const [file, setFile] = useState("");
+import {addDoc, collection, doc, setDoc, serverTimestamp} from "firebase/firestore";
+import {auth, db} from "../../firebase";
 
-  return (
-    <div className="new">
-      <Sidebar />
-      <div className="newContainer">
-        <Navbar />
-        <div className="top">
-          <h1>{title}</h1>
-        </div>
-        <div className="bottom">
-          <div className="left">
-            <img
-              src={
-                file
-                  ? URL.createObjectURL(file)
-                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-              }
-              alt=""
-            />
-          </div>
-          <div className="right">
-            <form>
-              <div className="formInput">
-                <label htmlFor="file">
-                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
-                </label>
-                <input
-                  type="file"
-                  id="file"
-                  onChange={(e) => setFile(e.target.files[0])}
-                  style={{ display: "none" }}
-                />
-              </div>
+import {createUserWithEmailAndPassword} from "firebase/auth";
 
-              {inputs.map((input) => (
-                <div className="formInput" key={input.id}>
-                  <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+
+const New = ({inputs, title}) => {
+    const [file, setFile] = useState("");
+    const [data, setData] = useState({})
+
+    const handleAdd = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await createUserWithEmailAndPassword(auth, data.email, data.password);
+
+            await setDoc(doc(db, "users", res.user.uid), {
+                ...data,
+                timeStamp: serverTimestamp(),
+            });
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
+    const handleInput = (e) => {
+        const id = e.target.id
+        const value = e.target.value
+
+        setData({...data, [id]: value})
+    }
+
+    console.log(data)
+
+    return (
+        <div className="new">
+            <Sidebar/>
+            <div className="newContainer">
+                <Navbar/>
+                <div className="top">
+                    <h1>{title}</h1>
                 </div>
-              ))}
-              <button>Send</button>
-            </form>
-          </div>
+                <div className="bottom">
+                    <div className="left">
+                        <img
+                            src={
+                                file
+                                    ? URL.createObjectURL(file)
+                                    : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                            }
+                            alt=""
+                        />
+                    </div>
+                    <div className="right">
+                        <form onSubmit={handleAdd}>
+                            <div className="formInput">
+                                <label htmlFor="file">
+                                    Image: <DriveFolderUploadOutlinedIcon className="icon"/>
+                                </label>
+                                <input
+                                    type="file"
+                                    id="file"
+                                    onChange={(e) => setFile(e.target.files[0])}
+                                    style={{display: "none"}}
+                                />
+                            </div>
+
+                            {inputs.map((input) => (
+                                <div className="formInput" key={input.id}>
+                                    <label>{input.label}</label>
+                                    <input
+                                        id={input.id}
+                                        type={input.type}
+                                        placeholder={input.placeholder}
+                                        onChange={handleInput}/>
+                                </div>
+                            ))}
+                            <button type="submit">Send</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default New;
